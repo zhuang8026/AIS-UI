ec6109e26b4bf5e4dc538b3f145c8b051102665c
Wiiliam_Chuang <william_chuang@asus.com>
2023-01-09 11:45:44 +0800

    'commit'

--- src/components/UiButton/index.vue
+++ src/components/UiButton/index.vue
@@ -23,7 +23,7 @@ export default {
   },
   methods: {
     clickBtn() {
-      alert('click, thx!');
+      alert('click, thx! ');
     },
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-/*Thu Dec 29 2022 10:55:52 GMT+0800 (台北標準時間)*/import UiButton from './components/UiButton';import UiInput from './components/UiInput';export { UiButton,UiInput }; 
+/*Mon Jan 09 2023 11:45:08 GMT+0800 (台北標準時間)*/import UiButton from './components/UiButton';import UiInput from './components/UiInput';export { UiButton,UiInput }; 
