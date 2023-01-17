1e9367d280ae6101894326540fdc537da90ea6bd
cerise_huang <cerise_huang@asus.com>
2023-01-17 09:24:47 +0800

    Add svg-sprite-loader  setting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
+import './assets/scss/style-global.scss';
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import './index.css'
+import PicIcon from './components/Icon/index.vue';
+import vSelect from 'vue-select'
 // import store from "./store";
 
-createApp(App)
+let app = createApp(App).use(router)
+
+//iCon component register globally
+const requireAll = (requireContext) => requireContext.keys().map(requireContext);
+const req = require.context('@/assets/icon', false, /.svg$/);
+requireAll(req);
+app.component('Icon', PicIcon)
+
+
+//替換掉原本的下拉選單下拉icon
+// import OpenIndicator from '@/components/Icon/OpenIndicator.vue' //原本的icon長相
+// import SelectIcon from '@/components/Icon/SelectIcon.vue' //新的icon長相
+
+// vSelect.props.components.default = () => ({ OpenIndicator, SelectIcon });
+// app.component('v-select', vSelect)
+
+
 // .use(store)
-.use(router)
-.mount("#app");
+
+app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
 module.exports = {
   lintOnSave: false,
   css: {
@@ -16,11 +19,25 @@ module.exports = {
       },
     },
   },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.join(__dirname, 'src'),
-      },
-    },
-  },
+  chainWebpack: config => {
+  
+    // 先刪除預設的svg配置
+    config.module.rules.delete("svg")
+    
+    // 新增 svg-sprite-loader 設定
+    config.module
+      .rule("svg-sprite-loader") 
+      .test(/\.svg$/)
+      .include
+      .add(resolve("src/assets/icon"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({ symbolId: "[name]" })
+      
+    // 修改 images-loader 配置
+    config.module
+      .rule("images")
+      .exclude.add(resolve("src/assets/icon"))
+  }
 };
