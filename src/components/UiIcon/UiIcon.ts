3e103f7b5115b56bb6223557ade65cf4516978cf
Lynn <lynn1_chen@asus.com>
2023-01-29 13:57:06 +0800

    update css setting

---
+++ src/components/UiIcon/UiIcon.ts
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'ui-icon',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
---
+++ src/components/UiIcon/index.vue
@@ -0,0 +1,4 @@
+<template src="./UiIcon.html"></template>
+<script lang="ts" src="./UiIcon.ts"></script>
+<style src="./UiIcon.scss" scoped lang="scss"></style>
+
