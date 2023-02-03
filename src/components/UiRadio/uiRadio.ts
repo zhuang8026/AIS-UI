a553b24987b7ae89294679df8230beac96b97fa9
cerise_huang <cerise_huang@asus.com>
2023-02-03 14:48:52 +0800

    Optimize radio

---
+++ src/components/UiRadio/uiRadio.ts
@@ -0,0 +1,78 @@
+
+import { reactive, computed, watch } from 'vue'
+
+export default {
+  components: {
+  },
+  props: {
+    error: String,
+    displayColumn: String,
+    optionDisplayColumn: {
+      type: Boolean,
+      required: false,
+      default:false,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    label:  {
+      type: String,
+      required: false,
+    },
+    value: {
+      type: [String, Boolean, Number],
+      required: true,
+    },
+    // theme:{ //目前沒有其他樣式
+    //   type: String,
+    //   required: false,
+    //   default : ''
+    // },
+    size:{ //default '' 20px, small 10px
+      type: String,
+      required: false,
+      default : ''
+    },
+    required: Boolean,
+    show: { //只用來展示，不能點擊，也沒有禁止的cursor
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    margin:{ //控制 每個 checkbox 間隔 例如 : '0 0 20px 0'
+      type: String,
+      required: false,
+      default : ''
+    }
+  },
+  setup(props: { value?: any; text?: any; layout?: any; size?: any; isError?: any; isDisable?: any; isSlot?: any; },  { emit }: any) {
+    // const {  text, layout, size, isError, isDisable, isSlot } = props;
+    
+    const state = reactive({
+      
+
+    })
+    
+    
+    const radioValue = computed({
+      get: () => props.value,
+      set: (val) => {
+        
+        emit('update:value', val);
+
+        emit('changeValue', val);
+
+      }
+    });
+
+
+    return {
+      state,
+      radioValue,
+    }
+
+  }// end: setup
+}
+
+
--- src/page/RadioView.vue
+++ src/page/RadioView.vue
@@ -2,57 +2,122 @@
   <div class="home">
     <h2>Radio btn</h2>
     <hr/>
-    <h3>Normal</h3>
-    <ui-radio
-      name="radio_1"
-      value="1"
-      v-model:checked="checked"
-      @checkChange="onChangeRadio($event)"
-    >Normal 1 -> {{ checked }}</ui-radio>
-    <ui-radio
-      name="radio_2"
-      value="2"
-      v-model:checked="checked"
-      @checkChange="onChangeRadio($event)"
-    >Normal 2 -> {{ checked }}</ui-radio>
-    <hr/>
-    <h3>Disable</h3>
-    <ui-radio
-      name="radio_3"
-      value="3"
-      :disabled="disabled"
-      v-model:checked="checked_d"
-      @checkChange="onChangeRadio($event)"
-    >Normal 3 -> {{ checked_d }}</ui-radio>
-    <ui-radio
-      name="radio_4"
-      value="4"
-      :disabled="disabled"
-      v-model:checked="checked_d"
-      @checkChange="onChangeRadio($event)"
-    >Normal 4 -> {{ checked_d }}</ui-radio>
+    <h2 class="my-14px">Normal</h2>
+    <div class="flex">
+      <Radio class="mt-5px" :options="validList" displayColumn="h" 
+        label="" v-model:value="check" @changeValue="isCheck"></Radio>
+        <Radio class="mt-30px" :options="validList" 
+        label="" v-model:value="check1" @changeValue="isCheck"></Radio>
+    </div>
+    <hr class="bg-grey-97 my-14px">
+    <h2 class="my-14px">Small </h2>
+    <div class="flex">
+      <Radio class="mt-5px" :options="validList" displayColumn="h" 
+        label="" v-model:value="check2" @changeValue="isCheck" size="small"></Radio>
+        <Radio class="mt-30px" :options="validList" 
+        label="" v-model:value="check3" @changeValue="isCheck" size="small"></Radio>
+    </div>
+
+    <hr class="bg-grey-97 my-14px">
+    <h2  class="my-14px">Show 展示用，不能勾也沒有cursor </h2>
+    <div class="flex">
+      <Radio class="mt-5px" :options="validList" displayColumn="h" 
+        label="" v-model:value="check4" @changeValue="isCheck" show="true"></Radio>
+        <Radio class="mt-30px" :options="validList" 
+        label="" v-model:value="check5" @changeValue="isCheck" size="small" show="true"></Radio>
+    </div>
+
+
+    <hr class="bg-grey-97 my-14px">
+    <h2  class="my-14px">Disable &  Disable卻被預設勾選</h2>
+    <div class="flex">
+      <Radio class="mt-5px" :options="validList2" displayColumn="h" 
+        label="" v-model:value="check6" @changeValue="isCheck" ></Radio>
+        <Radio class="mt-30px" :options="validList2" 
+        label="" v-model:value="check7" @changeValue="isCheck" ></Radio>
+    </div>
+
+
+
+    <!-- <hr class="bg-grey-97 my-14px">
+    <h2  class="my-14px">綜合</h2>
+    <div class="flex">
+      <Radio class="mt-5px" :options="validList2" displayColumn="h" 
+        label="" v-model:value="check8" @changeValue="isCheck"  theme="line" size="small" ></Radio>
+        <Radio class="mt-30px" :options="validList2" 
+        label="" v-model:value="check9" @changeValue="isCheck" theme="line" ></Radio>
+    </div> -->
   </div>
 </template>
 
 <script lang="ts">
-  import UiRadio from '@/components/UiRadio/index.vue'; // @ is an alias to /src
+  import Radio from '@/components/UiRadio/index.vue'; // @ is an alias to /src
 
   export default {
     name: 'RadioView',
     components: {
-      UiRadio,
+      Radio,
     },
     data() {
       return {
-        checked: '1',
-        checked_d: '4',
-        disabled: true,
+        validList:  [
+        {
+          name:  '8-16characters',
+          val: '0',
+          disabled: false,
+        },
+        {
+          name:  'include numbers',
+          val: '1',
+          disabled: false,
+        },
+        {
+          name: 'include uppercase and lowercase',
+          val: '2',
+          disabled: false,
+        },
+        {
+          name: 'include special characters',
+          val: '3',
+          disabled: false,
+        }
+      ],
+
+      validList2:  [
+        {
+          name:  'aaaaaaa',
+          val: '0',
+          disabled: true,
+        },
+        {
+          name:  'bbbbbbb',
+          val: '1',
+          disabled: true,
+        },
+        {
+          name: 'gggggggg',
+          val: '2',
+          disabled: true,
+        },
+        {
+          name: 'hhhhhhh',
+          val: '3',
+          disabled: false,
+        }
+      ],
+        check: '1',
+        check1: '3',
+        check2: '0',
+        check3: '2',
+        check4: '3',
+        check5: '1',
+        check6: '1',
+        check7: '',
+        check8: '',
+        check9: '',
       };
     },
     methods: {
-      onChangeRadio(val: any) {
-        console.log('onChangeRadio:', val);
-      },
     },
   };
 </script>
